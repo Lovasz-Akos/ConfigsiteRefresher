b9adcae10acc8e8ba4bf943d88bb5efc5fa22772
Naara <akos.lovasz@chameleon.sh>
2023-11-02 22:22:20 +0100

    fix dns address

--- tokenRefresher.user.js
+++ tokenRefresher.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Keep Token Alive Chameleon
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Token expires after 1 hour, so refresh the page once every (bit under an hour)
 // @author       Naara
 // @match        http://192.168.0.111:9000/*
 // @match        http://localhost:9000/*
-// @match        http://https://naara.dns.chameleon.sh/*
+// @match        *://naara.dns.chameleon.sh/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=0.111
 // @grant        none
 // @updateURL    https://github.com/Lovasz-Akos/ConfigsiteRefresher/raw/main/tokenRefresher.user.js
